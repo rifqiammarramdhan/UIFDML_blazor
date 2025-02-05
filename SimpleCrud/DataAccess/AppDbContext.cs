c1b0e638d4d375eedce7faa2b9c48669cbf8da1f
Ammar <rifqiammarramadhan103@gmail.com>
2025-02-05 16:23:19 +0700

    Add project files.

---
+++ SimpleCrud/DataAccess/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCrud.DataAccess.Entities;
+
+namespace SimpleCrud.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.GenerateSeed();
+        }
+    }
+}
---
+++ SimpleCrud/DataAccess/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleCrud.DataAccess.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FullName { get; set; } = default!;
+        public string Department { get; set; } = default!;
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; } = default!;
+    }
+}
